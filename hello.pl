e76eadcd7bba55adb3f0d6cf9bd5e37a9ae210ab
funinemo@gmail.com <funinemo@gmail.com>
2017-05-30 22:37:34 +0900

    Create hello.pl

---
+++ hello.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+print "hello world\n";
+<<<<<<< HEAD
+
+my $s = "hello world";
+print $s,"\n";
+exit;
+=======
+>>>>>>> 6e0a19d8397cd3f7882e9a0b8d4e78b98d516fdc
